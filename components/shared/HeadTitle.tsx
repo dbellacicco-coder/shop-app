03896c6488796d49c302ac9be167549070a0b4c5
dbellacicco-coder <dgbc1827@gmail.com>
2023-11-09 22:22:26 +0100

    set up project

---
+++ components/shared/HeadTitle.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Head from "next/head";
+
+interface HeadTitleProps {
+  title: string;
+}
+
+const HeadTitle: React.FC<HeadTitleProps> = ({ title }) => {
+  return (
+    <Head>
+      <title>{title}</title>
+    </Head>
+  );
+};
+
+export default HeadTitle;
